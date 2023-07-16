d40199131561e8199422be7ec9a410676b119742
ekaterina balabanidis <mythos.kath.nidis@gmail.com>
2023-07-16 19:04:32 +0300

    feat: реализовать временное решение авторизации

--- app.js
+++ app.js
@@ -4,7 +4,18 @@ const { PORT = 3000 } = process.env;
 
 const mongoose = require('mongoose');
 
+const bodyParser = require('body-parser');
+
 const app = express();
+
+app.use(bodyParser.json());
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: '64b3dce9019b9e414ccfb356',
+  };
+  next();
+});
 
 app.get('/', (_req, res) => {
   res.json({ response: 'success' });
@@ -21,5 +32,5 @@ app.use('/users', require('./routes/users'));
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
 
 app.listen(PORT, () => {
-  console.log(`Приложение работает на ${PORT} порту`);
+  console.log(`Приложение работает на ${PORT} порте`);
 });
--- routes/users.js
+++ routes/users.js
@@ -6,10 +6,12 @@
 // PATCH /users/me/avatar — обновляет аватар
 const router = require('express').Router();
 
-const { createUser, getUsers } = require('../controllers/users');
+const { createUser, getUsers, getUser } = require('../controllers/users');
 
 router.post('/', createUser); // было "/users" но я убрала, т.к. в app уже стоит /users. Проверить на работоспособность
 
 router.get('/', getUsers);
+
+router.get('/:id', getUser);
 
 module.exports = router;
