78146cb1645931f8ea825ae4a30e31dfdd566159
ekaterina balabanidis <mythos.kath.nidis@gmail.com>
2023-08-03 20:25:06 +0300

    feat: добавить валидацию Joi email и пароля при авторизаии

--- app.js
+++ app.js
@@ -17,7 +17,13 @@ const app = express();
 
 app.use(bodyParser.json());
 
-app.post('/signin', login);
+app.post('/signin', celebrate({
+  body: Joi.object().keys({
+    email: Joi.string().required().email(),
+    password: Joi.string().required(),
+  }),
+}), login);
+
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
