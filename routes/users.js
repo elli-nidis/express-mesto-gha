33ea628177c7bc177df182c26a2d8c3b3e1480dd
ekaterina balabanidis <mythos.kath.nidis@gmail.com>
2023-08-03 22:19:16 +0300

    feat: добавить валидацию Joi в роуте patch users/:me

--- routes/users.js
+++ routes/users.js
@@ -12,7 +12,12 @@ router.get('/:userId', celebrate({
     userId: Joi.string().required().hex().length(24),
   }),
 }), getUser);
-router.patch('/me', updateUser);
+router.patch('/me', celebrate({
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }).unknown(true),
+}), updateUser);
 router.patch('/me/avatar', celebrate({
   body: Joi.object().keys({
     avatar: Joi.string().required().pattern(/https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z]{1,4}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*)/),
