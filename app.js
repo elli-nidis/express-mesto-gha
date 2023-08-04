c3eb009c2e48662a3ebde8e0928d9af3e4c3ccbf
ekaterina balabanidis <mythos.kath.nidis@gmail.com>
2023-08-04 20:03:04 +0300

    fix: испрасить ошибку линта: добавить пустую строку

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const helmet = require('helmet');
+
 const { PORT = 3000 } = process.env;
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
